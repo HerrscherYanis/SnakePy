f7329713011b6e9f33e8609a0c25b6a47dda8ef6
yanis lamliji <yanis.lamliji@ynov.com>
2025-01-08 15:58:54 +0100

    game

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
-import machine, neopixel, time, random
+import depend
+#from Class import
 mapsint = 64
-up = machine.Pin(4, machine.Pin.IN, machine.Pin.PULL_UP)
-down = machine.Pin(5, machine.Pin.IN, machine.Pin.PULL_UP)
-left = machine.Pin(6, machine.Pin.IN, machine.Pin.PULL_UP)
-right = machine.Pin(7, machine.Pin.IN, machine.Pin.PULL_UP)
-np = neopixel.NeoPixel(machine.Pin(3), mapsint)
+up = depend.machine.Pin(4, depend.machine.Pin.IN, depend.machine.Pin.PULL_UP)
+down = depend.machine.Pin(5, depend.machine.Pin.IN, depend.machine.Pin.PULL_UP)
+left = depend.machine.Pin(6, depend.machine.Pin.IN, depend.machine.Pin.PULL_UP)
+right = depend.machine.Pin(7, depend.machine.Pin.IN, depend.machine.Pin.PULL_UP)
+np = depend.neopixel.NeoPixel(depend.machine.Pin(3), mapsint)
 
 def Pri(color):
     if(color[1] == None):
@@ -16,21 +17,53 @@ def Pri(color):
     
 def Element():
     grass = list((mapsint,None,(0, 1, 0)))
-    sn = list((1,27,(0,1,2)))
-    apple = list((1,AppleRan(sn, None),(2,0,0)))
+    sn = list((1,27,(0,1,2),0))
+    apple = list((1,AppleRan(sn, None),(2,0,0),"ordinary"))
     print(apple)
     return apple,grass,sn
 
 
 def AppleRan(sn, appleco):
     while True:
-        apple=random.randint(0, mapsint-1)
+        apple=depend.random.randint(0, mapsint-1)
         if(apple!=sn):
             if(appleco == None):
                 return apple
             else:
                 appleco[1] = apple
-                return appleco 
+                return appleco
+
+def Quality(apple,sn):
+    rand = depend.random.randint(0, 9)
+    if(rand == 0):
+        apple[3] = "poisoned"
+        apple[2] = (2,2,3)
+        return apple
+    elif(rand == 1):
+        apple[3] = "golden"
+        apple[2] = (2,2,0)
+        return apple
+    #elif(rand == 2 and sn[0] > 1):
+        #apple[3] = "laxative"
+        #apple[2] = (2,1,2)
+        #return apple
+    elif(rand == 3):
+        apple[3] = "hiden"
+        apple[2] = (0,3,0)
+        return apple
+    elif(rand == 4):
+        apple[3] = "evil"
+        apple[2] = (0,0,0)
+        return apple
+    elif(rand == 5):
+        apple[3] = "angel"
+        apple[2] = (0,0,0)
+        return apple
+    else:
+        apple[3] = "ordinary"
+        apple[2] = (2,0,0)
+        return apple
+              
 
 def Move(dir):
     if(up.value()==0):
@@ -76,7 +109,6 @@ def Snake(dir, sn):
     elif(dir=="right"):
         for t in range(8):
             if(sn>=r[t-1] and sn<=l[t-1]):
-                print(t%2)
                 if(t%2==0):
                     for t in range(8):
                         if(sn==r[t]):
@@ -91,7 +123,6 @@ def Snake(dir, sn):
     elif(dir=="left"):
         for t in range(8):
             if(sn>=r[t-1] and sn<=l[t-1]):
-                print(t%2)
                 if(t%2==0):
                     for t in range(8):
                         if(sn==l[t]):
@@ -118,10 +149,35 @@ def Start(apple,grass,sn):
     Pri(apple)
     Pri(sn)
     
+def Effect(sn,apple):
+    if(apple[3] == "ordinary"):
+        sn[3] = sn[3]+1
+        sn[0] = sn[0]+1
+    elif(apple[3] == "golden"):
+        sn[3] = sn[3]+5
+        sn[0] = sn[0]+1
+    elif(apple[3] == "laxative"):
+        sn[0] = sn[0]-1
+    elif(apple[3] == "hiden"):
+        sn[3] = sn[3]+5
+        sn[0] = sn[0]+1
+    elif(apple[3] == "poisoned"):
+        sn[3] = sn[3]-10
+        sn[0] = sn[0]+0
+    elif(apple[3] == "evil"):
+        sn[3] = sn[3]-30
+        sn[0] = sn[0]+1
+    elif(apple[3] == "angel"):
+        sn[3] = sn[3]+30
+        sn[0] = sn[0]+0
+    #elif(apple[3] == "cherry"):
+        #sn[0] = sn[0]+1
+        
 def Eat(sn,apple):
     if(apple[1]==sn[1]):
+        Effect(sn,apple)
         apple = AppleRan(sn, apple)
-        sn[0] = sn[0]+1
+        apple = Quality(apple,sn)
         Pri(apple)
 
 def Lose(snize, sn):
@@ -131,22 +187,38 @@ def Lose(snize, sn):
 
 def GameOver():
     Pri(list((mapsint,None,(0, 1, 0))))
-    time.sleep(1)
+    depend.time.sleep(1)
     Pri(list((mapsint,None,(1, 1, 0))))
 
-def Start():
+def deathpown(timer,apple,grass,sn):
+    timer= timer+1
+    if(timer>20 and timer < 30):
+        Pri(apple)
+        depend.time.sleep(0.1)
+        Pri((None, apple[1], grass[2]))
+    elif(timer>30):
+        timer = 0
+        apple = AppleRan(sn, apple)
+        apple = Quality(apple,sn)
+        Pri(apple)
+        print(apple)
+    return timer
+        
+def StartMain():
     apple, grass, sn = Element()
     Start(apple,grass,sn)
     dir="right"
     snize =[sn[1]]
+    timer= 0;
     while True:
-        time.sleep(0.1)
         Eat(sn,apple)
         Lesn(sn,snize,grass)
         dir = Move(dir)
         sn[1] = Snake(dir, sn[1])
-        print(sn[1])
+        depend.time.sleep(0.3)
+        timer = deathpown(timer,apple,grass,sn)
         if(Lose(snize, sn)==True):
             break
 print("run")
+StartMain()
 GameOver()
--- Song.py
+++ Song.py
@@ -1,119 +1,107 @@
 from machine import Pin,PWM
-import time
+from utime import sleep_ms
 
-tones = {
-"B0": 31,
-"C1": 33,
-"CS1": 35,
-"D1": 37,
-"DS1": 39,
-"E1": 41,
-"F1": 44,
-"FS1": 46,
-"G1": 49,
-"GS1": 52,
-"A1": 55,
-"AS1": 58,
-"B1": 62,
-"C2": 65,
-"CS2": 69,
-"D2": 73,
-"DS2": 78,
-"E2": 82,
-"F2": 87,
-"FS2": 93,
-"G2": 98,
-"GS2": 104,
-"A2": 110,
-"AS2": 117,
-"B2": 123,
-"C3": 131,
-"CS3": 139,
-"D3": 147,
-"DS3": 156,
-"E3": 165,
-"F3": 175,
-"FS3": 185,
-"G3": 196,
-"GS3": 208,
-"A3": 220,
-"AS3": 233,
-"B3": 247,
-"C4": 262,
-"CS4": 277,
-"D4": 294,
-"DS4": 311,
-"E4": 330,
-"F4": 349,
-"FS4": 370,
-"G4": 392,
-"GS4": 415,
-"A4": 440,
-"AS4": 466,
-"B4": 494,
-"C5": 523,
-"CS5": 554,
-"D5": 587,
-"DS5": 622,
-"E5": 659,
-"F5": 698,
-"FS5": 740,
-"G5": 784,
-"GS5": 831,
-"A5": 880,
-"AS5": 932,
-"B5": 988,
-"C6": 1047,
-"CS6": 1109,
-"D6": 1175,
-"DS6": 1245,
-"E6": 1319,
-"F6": 1397,
-"FS6": 1480,
-"G6": 1568,
-"GS6": 1661,
-"A6": 1760,
-"AS6": 1865,
-"B6": 1976,
-"C7": 2093,
-"CS7": 2217,
-"D7": 2349,
-"DS7": 2489,
-"E7": 2637,
-"F7": 2794,
-"FS7": 2960,
-"G7": 3136,
-"GS7": 3322,
-"A7": 3520,
-"AS7": 3729,
-"B7": 3951,
-"C8": 4186,
-"CS8": 4435,
-"D8": 4699,
-"DS8": 4978
-}
+# fréquence en Hz des notes utiles
+NOTE_B0 = 31
+NOTE_C1 = 33
+NOTE_CS1 = 35
+NOTE_D1 = 37
+NOTE_DS1 = 39
+NOTE_E1 = 41
+NOTE_F1 = 44
+NOTE_FS1 = 46
+NOTE_G1 = 49
+NOTE_GS1 = 52
+NOTE_A1 = 55
+NOTE_AS1 = 58
+NOTE_B1 = 62
+NOTE_C2 = 65
+NOTE_CS2 = 69
+NOTE_D2 = 73
+NOTE_DS2 = 78
+NOTE_E2 = 82
+NOTE_F2 = 87
+NOTE_FS2 = 93
+NOTE_G2 = 98
+NOTE_GS2 = 104
+NOTE_A2 = 110
+NOTE_AS2 = 117
+NOTE_B2 = 123
+NOTE_C3 = 131
+NOTE_CS3 = 139
+NOTE_D3 = 147
+NOTE_DS3 = 156
+NOTE_E3 = 165
+NOTE_F3 = 175
+NOTE_FS3 = 185
+NOTE_G3 = 196
+NOTE_GS3 = 208
+NOTE_A3 = 220
+NOTE_AS3 = 233
+NOTE_B3 = 247
+NOTE_C4 = 262
+NOTE_CS4 = 277
+NOTE_D4 = 294
+NOTE_DS4 = 311
+NOTE_E4 = 330
+NOTE_F4 = 349
+NOTE_FS4 = 370
+NOTE_G4 = 392
+NOTE_GS4 = 415
+NOTE_A4 = 440
+NOTE_AS4 = 466
+NOTE_B4 = 494
+NOTE_C5 = 523
+NOTE_CS5 = 554
+NOTE_D5 = 587
+NOTE_DS5 = 622
+NOTE_E5 = 659
+NOTE_F5 = 698
+NOTE_FS5 = 740
+NOTE_G5 = 784
+NOTE_GS5 = 831
+NOTE_A5 = 880
+NOTE_AS5 = 932
+NOTE_B5 = 988
+NOTE_C6 = 1047
+NOTE_CS6 = 1109
+NOTE_D6 = 1175
+NOTE_DS6 = 1245
+NOTE_E6 = 1319
+NOTE_F6 = 1397
+NOTE_FS6 = 1480
+NOTE_G6 = 1568
+NOTE_GS6 = 1661
+NOTE_A6 = 1760
+NOTE_AS6 = 1865
+NOTE_B6 = 1976
+NOTE_C7 = 2093
+NOTE_CS7 = 2217
+NOTE_D7 = 2349
+NOTE_DS7 = 2489
+NOTE_E7 = 2637
+NOTE_F7 = 2794
+NOTE_FS7 = 2960
+NOTE_G7 = 3136
+NOTE_GS7 = 3322
+NOTE_A7 = 3520
+NOTE_AS7 = 3729
+NOTE_B7 = 3951
+NOTE_C8 = 4186
+NOTE_CS8 = 4435
+NOTE_D8 = 4699
+NOTE_DS8 = 4978
 
-def Test():
-    song = ["E5","G5","A5","P","E5","G5","B5","A5","P","E5","G5","A5","P","G5","E5"]
-    duration = [1000,1000,1000,0,1000,1000,1000,1000,0,1000,1000,1000,0,1000,1000]
-    if(len(song) == len(duration)):
-        print(len(song))
-        return len(song),song,duration
-    else:
-        return None
-
-def Play(music):
-    if(music == None):
-        print("Error")
-    else:
-        pwm0=PWM(Pin(9))
-        for t in range(music[0]):
-            if (music[1][t] == "P"):
-                time.sleep_ms(music[2][t])
-            else:
-                pwm0.freq(tones[music[1][t]])
-                pwm0.duty_u16(music[2][t])
-                time.sleep(0.3)
-        pwm0.duty_u16(0)
-a = Test()            
-Play(a)
+# les notes (hauteur et durée) de la mélodie à jouer
+Colodie = [ [NOTE_G3,1], [NOTE_G1,2],[NOTE_G2,2]]
+Game_Over= [[NOTE_D4,3], [NOTE_D3,3], [NOTE_D2,4],[NOTE_D1,7]]
+Test= [[NOTE_E4,1],[NOTE_E5,1],[NOTE_F5,1],[NOTE_F5,3],[NOTE_E4,1],[NOTE_E5,1],[NOTE_F5,1],[NOTE_F5,3],[NOTE_E5,1],[NOTE_E5,1],[NOTE_F5,1],[NOTE_D5,1],[NOTE_G5,2],[NOTE_A5,2],[NOTE_B5,2],[NOTE_A5,2],[NOTE_B5,2],[NOTE_G5,2],[NOTE_G4,2]]
+pwm = PWM(Pin(9,Pin.OUT))
+#[NOTE_A5,2]
+for i in Test:
+    duree = (i[1] * 200) - 50;  # durée de la note, en milliseconde
+    pwm.freq(i[0])  # réglage de la fréquence (hauteur de la note)
+    pwm.duty_u16(32512) # rapport cyclique 50% (production d'un son)
+    sleep_ms(int(duree))  # on attend la durée requise
+    pwm.duty_u16(0) # rapport cyclique nul (silence)
+    sleep_ms(50) # silence pendant 50 ms (pour bien séparer les notes)
